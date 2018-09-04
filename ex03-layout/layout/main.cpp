8317115faee83d4b18b5da0a08aa9eea679efb43
amscw <moskwin.a.va@gmail.com>
2018-09-04 15:35:44 +0300

    simple layout example

---
+++ ex03-layout/layout/main.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QtWidgets>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QWidget wgt;
+    wgt.setObjectName("top_wgt");
+    QPushButton *pcmdA = new QPushButton("A");
+    QPushButton *pcmdB = new QPushButton("B");
+    QPushButton *pcmdC = new QPushButton("C");
+
+    // Layout setup
+    QBoxLayout *pbxLayout = new QBoxLayout(QBoxLayout::LeftToRight);
+    pbxLayout->addWidget(pcmdA, 1); // слабо тянучий
+    pbxLayout->addWidget(pcmdB, 2); // средне тянучий
+    pbxLayout->addWidget(pcmdC, 3); // сильно тянучий
+
+    // добовать компоновку
+    wgt.setLayout(pbxLayout);
+    wgt.resize(450, 40);
+    wgt.show();
+
+    qDebug().nospace() << "a parent of pcmdA is " << pcmdA->parent()->objectName();
+
+    return a.exec();
+}
